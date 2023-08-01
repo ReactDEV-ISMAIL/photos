e1473f3483dd9086f88194fe1d67c817704e9093
BOUZIANE <bouziane.ism@gmail.com>
2023-08-01 09:41:46 +0100

    Photos

--- src/constants/Errors.const.tsx
+++
@@ -1,7 +0,0 @@
-export const ERRORS = {
-  OBLIGATORY: "Ce champs est obligatoire",
-  INVALID: "Valeur invalide",
-  INVALID_EMAIL: "Email  invalide",
-  ICE_INVALID: "ICE doit contenir 15 chiffres",
-  PATENTE_OR_RC_OBLIGATORY: "Patente ou RC est obligatoire",
-};
